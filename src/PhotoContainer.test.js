a0a70ae4a61c27b3ffb8beb205c75787e9c5f311
Xin Wang <wangxin00163@yahoo.co.uk>
2018-02-24 22:54:56 +0000

    Add two test to check like state alter the class name

--- src/PhotoContainer.test.js
+++ src/PhotoContainer.test.js
@@ -18,6 +18,20 @@ it('renders the initial like count', () => {
   expect(wrapper.find('.Likes').text()).toEqual('53');
 });
 
+describe('Liked state', () => {
+  it('should have class "fa-heart-o" if liked state is false', () => {
+      const wrapper = mount(<PhotoContainer data={photoCardsData[1]} />);
+      expect(wrapper.state('liked')).toEqual(false);
+      expect(wrapper.find('.Likes').hasClass('fa-heart-o'));
+    });
+
+  it('should have class "fa-heart" if liked state is true', () => {
+      const wrapper = mount(<PhotoContainer data={photoCardsData[0]} />);
+      expect(wrapper.state('liked')).toEqual(true);
+      expect(wrapper.find('.Likes').hasClass('fa-heart'));
+    });
+});
+
 describe('LikeClick', () => {
   it('clicking on unliked post, update the likes count and change the state to like', () => {
       const wrapper = mount(<PhotoContainer data={photoCardsData[1]} />);
